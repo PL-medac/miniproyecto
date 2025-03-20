df8f63db9f52a894e9276db600ffad8ff651e5f2
JLMedac <jmp0048@alu.medac.es>
2025-03-20 13:45:40 +0100

    add flutter proyect and supabase connection

---
+++ miniproyecto/android/app/src/main/kotlin/com/example/miniproyecto/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.miniproyecto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ miniproyecto/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  String supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
+  String supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'] ?? '';
+
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Farmacéutica',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('FarmacéuticApp')),
+        body: const Center(child: Text('Home Page')),
+      ),
+    );
+  }
+}
