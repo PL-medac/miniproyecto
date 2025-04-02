ab373e8faedd5ecbbc53d2b7f98b21fb2ee92228
PMLlamas <patri.llro@gmail.com>
2025-04-02 15:58:12 +0200

    UI settings

--- miniproyecto/lib/screens/dashboard_screen.dart
+++ miniproyecto/lib/screens/dashboard_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:miniproyecto/screens/home_screen.dart';
 import 'package:provider/provider.dart';
@@ -229,6 +230,8 @@ class _PageOfPageState extends State<PageOfPage> {
 }
 
 class StockPage extends StatelessWidget {
+  const StockPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: MedicamentosStockPage());
@@ -447,13 +450,153 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  bool valNotify1 = true;
+  bool valNotify2 = false;
+
+  onChangeFunction1(bool newValue1) {
+    setState(() {
+      valNotify1 = newValue1;
+    });
+  }
+  onChangeFunction2(bool newValue2) {
+    setState(() {
+      valNotify2 = newValue2;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ElevatedButton.icon(
-        onPressed: () {},
-        icon: Icon(Icons.favorite),
-        label: Text('Love'),
+    return Container(
+      padding: const EdgeInsets.all(24),
+      child: ListView(
+        children: [
+          SizedBox(height: 10),
+
+          Row(
+            children: [
+              Icon(Icons.account_circle_sharp),
+              SizedBox(width: 10),
+              Text(
+                "Account",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+
+          Divider(height: 20, thickness: 1),
+          SizedBox(height: 10),
+          buildAccountOption(context, "Change Password"),
+          buildAccountOption(context, "Language"),
+          buildAccountOption(context, "Privacy and Security"),
+          SizedBox(height: 40),
+          Row(
+            children: [
+              Icon(Icons.volume_up_rounded),
+              SizedBox(width: 10),
+              Text(
+                "Notifications",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          Divider(height: 20, thickness: 1),
+          SizedBox(height: 10),
+          buildNotificationOption("Theme Dark", valNotify1, onChangeFunction1),
+          buildNotificationOption("Notifications Active", valNotify2, onChangeFunction2),
+          SizedBox(height: 50),
+          Center(
+            child: OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)
+                )
+              ),
+              onPressed: (){},
+              child: Text("SIGN OUT", style: TextStyle(
+                fontSize: 16,
+                letterSpacing: 2.2,
+                color: Colors.black,
+              ),)
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Padding buildNotificationOption(
+    String title,
+    bool value,
+    Function onChangedMethod,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey[600],
+            ),
+          ),
+          Transform.scale(
+            scale: 0.7,
+            child: CupertinoSwitch(
+              value: value,
+              onChanged: (bool newValue) {
+                onChangedMethod(newValue);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  GestureDetector buildAccountOption(BuildContext context, String title) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text(title),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [Text("Option 1"), Text("Option 2")],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Close"),
+                ),
+              ],
+            );
+          },
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey[600],
+              ),
+            ),
+            Icon(Icons.arrow_forward_ios, color: Colors.grey),
+          ],
+        ),
       ),
     );
   }
