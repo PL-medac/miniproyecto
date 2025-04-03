1c3f1a35c93e5c620677f06bfd8435373eaebd20
Alvaro Rodriguez Chofles <arc0027@alu.medac.es>
2025-04-03 13:43:17 +0200

    Actualización interfaz

--- miniproyecto/lib/screens/home/home_screen.dart
+++ miniproyecto/lib/screens/home/home_screen.dart
@@ -5,7 +5,6 @@ import 'dashboard_screen.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -14,40 +13,12 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'PharmaStock',
         theme: ThemeData(
-          primaryColor: Color(0xFF085F63), // Color principal
-          scaffoldBackgroundColor: Color.fromARGB(
-            255,
-            240,
-            245,
-            249,
-          ), // Fondo de la app
+          primaryColor: Color(0xFF085F63),
+          scaffoldBackgroundColor: Color.fromARGB(255, 240, 245, 249),
           appBarTheme: AppBarTheme(
             backgroundColor: Color(0xFF085F63),
-            foregroundColor: Colors.white, // Color de los iconos y texto
+            foregroundColor: Colors.white,
           ),
-          /*elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ButtonStyle(
-              backgroundColor: WidgetStateProperty.all(
-                Color.fromARGB(255, 23, 109, 97),
-              ),
-              foregroundColor: WidgetStateProperty.all(Colors.white),
-              textStyle: WidgetStateProperty.all(
-                TextStyle(fontSize: 30, color: Colors.white),
-              ),
-            ),
-          ),
-          bottomNavigationBarTheme: BottomNavigationBarThemeData(
-            backgroundColor: Color(0xFF0A2832),
-            selectedItemColor: Colors.white, // Color del icono seleccionado
-            unselectedItemColor: Colors.grey, // Color de icono sin seleccionar
-          ),
-          navigationRailTheme: NavigationRailThemeData(
-            backgroundColor: Color(0xFF0A2832),
-            selectedIconTheme: IconThemeData(color: Colors.white),
-            unselectedIconTheme: IconThemeData(color: Colors.grey),
-            selectedLabelTextStyle: TextStyle(color: Colors.white),
-            unselectedLabelTextStyle: TextStyle(color: Colors.grey),
-          ),*/
         ),
         home: const MyHomePage(),
       ),
@@ -60,12 +31,13 @@ class MyAppState extends ChangeNotifier {
 
   void updateIndex(int newIndex) {
     selectedIndex = newIndex;
-    notifyListeners(); //notifica los widgets dependientes
+    notifyListeners(); // notifica los widgets dependientes
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -73,30 +45,56 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color(0xFF248F8D),
-          
-          title: Image.asset(
-            "assets/logo.png", // Ruta de la imagen en assets
-            height: 50, // Ajusta el tamaño según sea necesario
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF248F8D),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: IconButton(
+              onPressed: () {
+                debugPrint("Notificaciones");
+              },
+              icon: const Icon(Icons.email),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          // Espacio arriba del logo
+          SizedBox(height: 75),
+
+          // Logo encima de los enlaces
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Image.asset(
+              "assets/portada.png", // Ruta de la imagen en assets
+              height: 200, // Ajusta el tamaño del logo según sea necesario
+            ),
           ),
 
-          centerTitle: true,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 16.0),
-              child: IconButton(
-                onPressed: () {
-                  debugPrint("Notificaciones");
-                },
-                icon: Icon(Icons.email),
+          // Espacio debajo del logo
+          SizedBox(height: 15),
+
+          // Botones de navegación
+          ElevatedButtonExample(),
+
+          // Espacio para separar el contenido del footer
+          Spacer(), // Esto empuja el footer hacia abajo
+          // Footer básico
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            color: Colors.grey[200], // Color de fondo del footer
+            child: Center(
+              child: Text(
+                '© 2025 PharmaStock - Todos los derechos reservados',
+                style: TextStyle(color: Colors.black54, fontSize: 14),
               ),
             ),
-          ],
-        ),
-        body: ElevatedButtonExample(),
-      
+          ),
+        ],
+      ),
     );
   }
 }
@@ -105,72 +103,67 @@ class ElevatedButtonExample extends StatelessWidget {
   const ElevatedButtonExample({super.key});
 
   @override
- 
   Widget build(BuildContext context) {
     final ButtonStyle style = ElevatedButton.styleFrom(
-      backgroundColor: Color.fromARGB(255, 23, 109, 97),
+      backgroundColor: const Color.fromARGB(255, 23, 109, 97),
       foregroundColor: Colors.white,
-      textStyle: const TextStyle(fontSize: 18,),
+      textStyle: const TextStyle(fontSize: 18),
       minimumSize: const Size(400, 60),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
     );
 
     return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10), // Margen lateral
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              ElevatedButton(
-                style: style,
-                onPressed: () {
-                  //Provider.of<MyAppState>( context, listen: false,).updateIndex(1);
-                  context.read<MyAppState>().updateIndex(1);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => const PageOfPage()),
-                  );
-                },
-                child: const Text('Stock'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                style: style,
-                onPressed: () {
-                  //Provider.of<MyAppState>(context,listen: false, ).updateIndex(2);
-                  context.read<MyAppState>().updateIndex(2);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => const PageOfPage()),
-                  );
-                },
-                child: const Text('New Input'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                style: style,
-                onPressed: () {
-                  //Provider.of<MyAppState>(context,listen: false,).updateIndex(3);
-                  context.read<MyAppState>().updateIndex(3);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => const PageOfPage()),
-                  );
-                },
-                child: const Text('Settings'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                style: style,
-                onPressed: () {
-                  debugPrint("Exit");
-                },
-                child: const Text('Exit'),
-              ),
-            ],
-          ),
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ElevatedButton(
+              style: style,
+              onPressed: () {
+                context.read<MyAppState>().updateIndex(1);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const PageOfPage()),
+                );
+              },
+              child: const Text('Listado Medicamentos'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: style,
+              onPressed: () {
+                context.read<MyAppState>().updateIndex(2);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const PageOfPage()),
+                );
+              },
+              child: const Text('Gestión CRUD'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: style,
+              onPressed: () {
+                context.read<MyAppState>().updateIndex(3);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const PageOfPage()),
+                );
+              },
+              child: const Text('Ajustes'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: style,
+              onPressed: () {
+                debugPrint("Cerrar Sesión");
+              },
+              child: const Text('Cerrar Sesión'),
+            ),
+          ],
         ),
-     
+      ),
     );
   }
 }
