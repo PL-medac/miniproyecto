0da7ce098505ffd2094efdf4e225748a5ac53eca
Alvaro Rodriguez Chofles <arc0027@alu.medac.es>
2025-04-07 12:50:20 +0200

    Implementación interfaz login / register

--- miniproyecto/lib/pages/register_page.dart
+++ miniproyecto/lib/pages/register_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:miniproyecto/repository/auth_service.dart';
+import 'dart:ui'; // Necesario para el BackdropFilter
+
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -15,8 +17,10 @@ class _RegisterPageState extends State<RegisterPage> {
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
 
+  final Color primaryColor = const Color(0xFF248F8D);
+
   void singUp() async {
-    final email = _emailController.text;
+    final email = _emailController.text.trim();
     final password = _passwordController.text;
     final confirmPassword = _confirmPasswordController.text;
 
@@ -30,7 +34,6 @@ class _RegisterPageState extends State<RegisterPage> {
 
     try {
       await authService.signUpWithEmailPassword(email, password);
-
       Navigator.pop(context);
     } catch (e) {
       if (mounted) {
@@ -44,38 +47,178 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Registro")),
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 50),
+      body: Stack(
         children: [
-          TextField(
-            controller: _emailController,
-            decoration: const InputDecoration(labelText: "Email"),
-          ), // Email
-
-          TextField(
-            controller: _passwordController,
-            decoration: const InputDecoration(labelText: "Contraseña"),
-          ), // Password
-
-          TextField(
-            controller: _confirmPasswordController,
-            decoration: const InputDecoration(
-              labelText: "Confirmar contraseña",
+          // Fondo con la imagen desenfocada
+           // Imagen de fondo desenfocada
+          Positioned.fill(
+            child: Image.asset(
+              'assets/fondo.png', // Asegúrate de tener la imagen en assets
+              fit: BoxFit.cover,
             ),
-          ), // Confirm Password
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 7.0, sigmaY: 7.0), // Desenfoque
+              child: Container(
+                color: Colors.black.withOpacity(0.7), // Opacidad para mejorar contraste
+              ),
+            ),
+          ),
+          // Contenido principal
+          Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 36),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Logo
+                  Image.asset(
+                    'assets/portada.png',
+                    height: 120,
+                  ),
+                  const SizedBox(height: 20),
 
-          ElevatedButton(onPressed: singUp, child: const Text("Registrarse")),
+                  Text(
+                    "Registro",
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 40),
 
-          // Register Page link
-          GestureDetector(
-            onTap:
-                () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const RegisterPage()),
-                ),
-            child: const Center(
-              child: Text("¿No tienes una cuenta? Registrate"),
+                  // Email input
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.5, // 50% width
+                    child: TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(color: Colors.white), // Etiqueta blanca
+                        prefixIcon: Icon(Icons.email_outlined, color: primaryColor),
+                        filled: true, // Fondo
+                        fillColor: Colors.transparent, // Fondo transparente
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: Colors.white), // Borde blanco
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: Colors.white), // Borde blanco
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: primaryColor), // Borde color primario
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.white), // Texto blanco
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+                  // Password input
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.5, // 50% width
+                    child: TextField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Contraseña",
+                        labelStyle: TextStyle(color: Colors.white), // Etiqueta blanca
+                        prefixIcon: Icon(Icons.lock_outline, color: primaryColor),
+                        filled: true, // Fondo
+                        fillColor: Colors.transparent, // Fondo transparente
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: Colors.white), // Borde blanco
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: Colors.white), // Borde blanco
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: primaryColor), // Borde color primario
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.white), // Texto blanco
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+                  // Confirm Password input
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.5, // 50% width
+                    child: TextField(
+                      controller: _confirmPasswordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Confirmar contraseña",
+                        labelStyle: TextStyle(color: Colors.white), // Etiqueta blanca
+                        prefixIcon: Icon(Icons.lock_outline, color: primaryColor),
+                        filled: true, // Fondo
+                        fillColor: Colors.transparent, // Fondo transparente
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: Colors.white), // Borde blanco
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: Colors.white), // Borde blanco
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: primaryColor), // Borde color primario
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.white), // Texto blanco
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+
+                  // Register button
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.3, // 30% width
+                    padding: const EdgeInsets.symmetric(vertical: 12), // Padding vertical
+                    child: ElevatedButton(
+                      onPressed: singUp,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: primaryColor,
+                        foregroundColor: Colors.white,
+                        padding: const EdgeInsets.symmetric(vertical: 20), // Más padding para el botón
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: const Text(
+                        "Registrarse",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  // Navigate to login page
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context); // Navigate back to login page
+                    },
+                    child: Text(
+                      "¿Ya tienes una cuenta? Inicia sesión",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: primaryColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
