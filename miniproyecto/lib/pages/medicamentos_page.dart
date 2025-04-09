0390ea8237501e8b93b7bb8ce8e7deadd18e4f2a
PMLlamas <patri.llro@gmail.com>
2025-04-09 11:25:31 +0200

    implementar lista medicamentos de supabase con app

--- miniproyecto/lib/pages/medicamentos_page.dart
+++ miniproyecto/lib/pages/medicamentos_page.dart
@@ -1,94 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:miniproyecto/generated/l10n.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class MedicamentosStockPage extends StatelessWidget {
+class MedicamentosStockPage extends StatefulWidget {
+  @override
+  State<MedicamentosStockPage> createState() => _MedicamentosStockPageState();
+}
+
+class _MedicamentosStockPageState extends State<MedicamentosStockPage> {
   // Lista de medicamentos con parámetros predefinidos, cargar aqui los medicamentos desde la base de datos
-  final List<Map<String, String>> medicamentos = [
-    {
-      'nombre': 'Paracetamol',
-      'stock': '50',
-      'fecha_expiracion': '2025-06-01',
-      'precio': '5.00 €',
-      'categoria': 'Analgésico',
-      'laboratorio': 'Laboratorios X',
-      'imagen': '../../assets/medicamento.png',
-    },
-    {
-      'nombre': 'Ibuprofeno',
-      'stock': '30',
-      'fecha_expiracion': '2025-05-15',
-      'precio': '7.00 €',
-      'categoria': 'Antiinflamatorio',
-      'laboratorio': 'Laboratorios Y',
-      'imagen': '../../assets/medicamento.png',
-    },
-    {
-      'nombre': 'Amoxicilina',
-      'stock': '20',
-      'fecha_expiracion': '2025-07-10',
-      'precio': '10.00 €',
-      'categoria': 'Antibiótico',
-      'laboratorio': 'Laboratorios Z',
-      'imagen': '../../assets/medicamento.png',
-    },
-  ];
+  List<dynamic> medicamentos = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMedicamentos();
+  }
+
+  Future<void> fetchMedicamentos() async {
+    final supabase = Supabase.instance.client;
+    final response = await supabase.from('products').select();
+    setState(() {
+      medicamentos = response;
+      isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(S.of(context).page1)),
-      body: ListView.builder(
-        itemCount: medicamentos.length,
-        itemBuilder: (context, index) {
-          return Card(
-            margin: EdgeInsets.all(10),
-            child: ListTile(
-              contentPadding: EdgeInsets.all(
-                10,
-              ), // Espaciado alrededor del contenido
-              leading:
-                  medicamentos[index]['imagen']!.isEmpty
-                      ? Icon(
-                        Icons.medical_services,
-                        size: 100, // Ajusta el tamaño del ícono
-                      )
-                      : Image.asset(
-                        medicamentos[index]['imagen']!,
-                        height: 100, // Ajusta la altura de la imagen
-                        width: 100, // Ajusta el ancho de la imagen
+      body:
+          isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                itemCount: medicamentos.length,
+                itemBuilder: (context, index) {
+                  final item = medicamentos[index];
+                  return Card(
+                    margin: EdgeInsets.all(10),
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.all(
+                        10,
+                      ), // Espaciado alrededor del contenido
+                      leading:Icon(
+                                Icons.medical_services,
+                                size: 100, // Ajusta el tamaño del ícono
+                              ),
+                              title : Text(
+                              /*Image.asset(
+                                medicamentos[index]['imagen']!,
+                                height: 100, // Ajusta la altura de la imagen
+                                width: 100, // Ajusta el ancho de la imagen
+                              ),*/
+                              item['name']?? 'Sin nombre',
+                              style: const TextStyle(
+                                fontSize: 22, // Tamaño de fuente más grande
+                                fontWeight: FontWeight.bold, // Texto en negrita
+                              ),
                       ),
-              title: Text(
-                medicamentos[index]['nombre']!,
-                style: TextStyle(
-                  fontSize: 22, // Tamaño de fuente más grande
-                  fontWeight: FontWeight.bold, // Texto en negrita
-                ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          
+                                Text(
+                                  '${S.of(context).stock}: ${medicamentos[index]['stock']}',
+                                ),
+                                Text(
+                                  '${S.of(context).expiration_date}: ${medicamentos[index]['expiration_date']}',
+                                ),
+                                Text(
+                                  '${S.of(context).price}: ${medicamentos[index]['price']}',
+                                ),
+                                Text(
+                                  '${S.of(context).category}: ${medicamentos[index]['id_category']}',
+                                ),
+                                Text(
+                                  '${S.of(context).laboratory}: ${medicamentos[index]['id_laboratory']}',
+                                ),
+                              ],
+                            ),
+                          ),
+                        
+                      );
+                    
+                },
               ),
-              subtitle: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('${S.of(context).stock}: ${medicamentos[index]['stock']}'),
-                        Text(
-                          '${S.of(context).expiration_date}: ${medicamentos[index]['fecha_expiracion']}',
-                        ),
-                        Text('${S.of(context).price}: ${medicamentos[index]['precio']}'),
-                        Text('${S.of(context).category}: ${medicamentos[index]['categoria']}'),
-                        Text(
-                          '${S.of(context).laboratory}: ${medicamentos[index]['laboratorio']}',
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
     );
   }
 }
